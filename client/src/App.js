d7ffabd81dafeb74ba8ab3c629e35cf2bcd30ca9
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-03-06 00:13:38 -0500

    Pete Lofstedt

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Bookmarks from "./components/Bookmarks";
 import HomeFeed from "./components/HomeFeed";
@@ -6,31 +6,49 @@ import Notifications from "./components/Notifications";
 import Profile from "./components/Profile";
 import TweetDetails from "./components/TweetDetails";
 import Sidebar from "./components/Sidebar";
+import { CurrentUserContext } from "./components/CurrentUserContext";
 
 const App = () => {
+  const { currentUser, setCurrentUser, status, setStatus } =
+    useContext(CurrentUserContext);
+
+  const receiveCurrentUserFromServer = (data) => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    console.log("useEffect useEffectin'");
+    fetch("/me/profile")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
-    <Router>
-      <div style={{ display: "flex" }}>
-        <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <HomeFeed />
-          </Route>
-          <Route exact path="/notifications/">
-            <Notifications />
-          </Route>
-          <Route exact path="/bookmarks/">
-            <Bookmarks />
-          </Route>
-          <Route path="/tweet/:tweetId">
-            <TweetDetails />
-          </Route>
-          <Route exact path="/:profileId">
-            <Profile />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      {/* {<GlobalStyles />} */}
+      <Router>
+        <div style={{ display: "flex" }}>
+          <Sidebar />
+          <Switch>
+            <Route exact path="/">
+              <HomeFeed />
+            </Route>
+            <Route exact path="/notifications/">
+              <Notifications />
+            </Route>
+            <Route exact path="/bookmarks/">
+              <Bookmarks />
+            </Route>
+            <Route path="/tweet/:tweetId">
+              <TweetDetails />
+            </Route>
+            <Route exact path="/:profileId">
+              <Profile />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 };
 
--- client/src/components/CurrentUserContext.js
+++ client/src/components/CurrentUserContext.js
@@ -5,8 +5,11 @@ export const CurrentUserContext = createContext(null);
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [status, setStatus] = useState("loading");
+
   return (
-    <CurrentUserContext.Provider value={{ currentUser, status }}>
+    <CurrentUserContext.Provider
+      value={{ currentUser, setCurrentUser, status, setStatus }}
+    >
       {children}
     </CurrentUserContext.Provider>
   );
